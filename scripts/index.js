7f44650d7c8ad22cabcbd910feb96eb2b60f98b2
stabarsik <stabarsik@gmail.com>
2023-09-05 11:23:34 +0200

    change gift animation to play when page's loaded

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,7 @@
-// Get a list of all svg elements
-icons = document.querySelectorAll('.icon-hook');
 
-// Cycle through list
-for (var i = 0; i < icons.length; i++) {
-  icons[i].addEventListener('click', function(event) {
-    event.preventDefault();
- 
-    var icon = this;
-    var currentClass = icon.getAttribute('class'); // The starting class
+window.addEventListener('load', function(event) {
 
-    console.log(icon);
-
-    if (currentClass.indexOf('active') > -1) { 
-      // Remove .active
-      icon.setAttribute('class', currentClass.replace(' active', ''));
-    } else { 
-      // Add .active
-      icon.setAttribute('class', currentClass + ' active');
-    }
-  }, false);
-}
+  const icon = document.querySelector('.icon-hook');
+  
+  icon.classList.add('active');
+}, false);
