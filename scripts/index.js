7556b07d7472740dcf87621b3b08c44e40feedbf
stabarsik <stabarsik@gmail.com>
2023-09-08 17:33:01 +0200

    add validating to the first screen

--- scripts/index.js
+++ scripts/index.js
@@ -19,38 +19,55 @@ window.addEventListener('load', function(event) {
 }, false);
 
 const loginForm = document.forms.loginForm;
+const inputValue = loginForm.elements.email;
+const inputError = loginForm.querySelector(`.${ inputValue.id}-error`);
+
+const showInputError = (element, errorMessage) => {
+  element.classList.add('form__input_type_error');
+  inputError.textContent = errorMessage;
+  inputError.classList.add('form__input-error_active');
+};
+
+// Функция, которая удаляет класс с ошибкой
+const hideInputError = (element) => {
+  element.classList.remove('form__input_type_error');
+  inputError.classList.remove('form__input-error_active');
+  inputError.textContent = "";
+};
+
+// Функция, которая проверяет валидность поля
+const isValid = (inputElement) => {
+  if (inputElement.validity.typeMismatch) {
+    showInputError(inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(inputElement);
+  }
+};
+
+// Вызовем функцию isValid на каждый ввод символа
+inputValue.addEventListener('change', () => { isValid(inputValue)}); 
 
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault();
-
-  const inputValue = loginForm.elements.email;
-
+  // isValid(inputValue);
   window.history.pushState({}, '', '/auth');
   // window.location.replace('/templates/auth.html');
-  urlLocationHandler(inputValue.value);
+  urlLocationHandler(inputValue);
 })
 
 
-const urlLocationHandler = async (inputValue) => {
-//   const location = window.location.pathname;
-  
-//   if (location.length === 0) {
-//     location = '/';
-//   }
-
-//   const route = Routes[location] || Routes[404];
-  const label = loginForm.querySelector('.login-form-label');
-  if (inputValue.length === 0)
-    return;
-  if (inputValue !== 'yula.bu@icloud.com') {
-    label.textContent = 'Ты не моя жена';
-    return;
+const urlLocationHandler = async (inputElement) => {
+  if (inputElement.validity.patternMismatch) {
+    // данные атрибута доступны у элемента инпута через ключевое слово dataset.
+    // обратите внимание, что в js имя атрибута пишется в camelCase (да-да, в
+    // HTML мы писали в kebab-case, это не опечатка)
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+    showInputError(inputElement, inputElement.validationMessage);
   }
-  const loginPage = document.querySelector('.login-page');
+
   const html = await fetch(Routes['/auth'].template).then((response) => response.text());
   console.log(html);
   // const elementCopy = html.cloneNode(true);
   // loginPage.append(html); 
-  document.getElementById('email').innerHTML = html;
-
+  // document.getElementById('email-input-error').innerHTML = html;
 }
