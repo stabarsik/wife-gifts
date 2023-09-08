cdb289c3e5079d0a060c5c788b8ab007f7e97cfd
stabarsik <stabarsik@gmail.com>
2023-09-08 13:43:30 +0200

    add styling to the first screen and playing with routing in JS (not finished yet)

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,15 @@
+const Routes = {
+  "/": {
+    template: 'templates/index.html',
+    title: '',
+    description: ''
+  },
+  "/auth": {
+    template: 'templates/auth.html',
+    title: '',
+    description: ''
+  }
+}
 
 window.addEventListener('load', function(event) {
 
@@ -5,3 +17,40 @@ window.addEventListener('load', function(event) {
   
   icon.classList.add('active');
 }, false);
+
+const loginForm = document.forms.loginForm;
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const inputValue = loginForm.elements.email;
+
+  window.history.pushState({}, '', '/auth');
+  // window.location.replace('/templates/auth.html');
+  urlLocationHandler(inputValue.value);
+})
+
+
+const urlLocationHandler = async (inputValue) => {
+//   const location = window.location.pathname;
+  
+//   if (location.length === 0) {
+//     location = '/';
+//   }
+
+//   const route = Routes[location] || Routes[404];
+  const label = loginForm.querySelector('.login-form-label');
+  if (inputValue.length === 0)
+    return;
+  if (inputValue !== 'yula.bu@icloud.com') {
+    label.textContent = 'Ты не моя жена';
+    return;
+  }
+  const loginPage = document.querySelector('.login-page');
+  const html = await fetch(Routes['/auth'].template).then((response) => response.text());
+  console.log(html);
+  // const elementCopy = html.cloneNode(true);
+  // loginPage.append(html); 
+  document.getElementById('email').innerHTML = html;
+
+}
